15502ca7b55161e8927ffa66a310d12990425e25
Sergey Korolev <korolevsv@gmail.com>
2014-08-08 17:48:33 +0400

    Storyboard updated

---
+++ SVKDatesPeriodViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVKDatesPeriodViewController.h
+//  CalCalcPush
+//
+//  Created by Sergey Korolev on 08.08.14.
+//  Copyright (c) 2014 SelfEducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVKDatesPeriodViewController : UIViewController
+
+@end
